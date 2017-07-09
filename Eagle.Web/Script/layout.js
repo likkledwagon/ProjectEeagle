8399500de3852d6f54976c5ba6e9df2560d20616
likkledwagon <bram.lefevre@gmail.com>
2017-07-10 00:47:37 +0200

    Styling

---
+++ Eagle.Web/Script/layout.js
@@ -0,0 +1,32 @@
+﻿function createBlueCallout(title, right, content, container) {
+
+    var elContainer = CreateDiv("blue-callout");
+    var byEm = createElement("em", "pull-right");
+    var titleEl = createElement("h4");
+    var contentEl = CreateDiv("");
+
+    byEm.html(right);
+    titleEl.html(title);
+    contentEl.html(content);
+
+    byEm.appendTo(elContainer);
+    titleEl.appendTo(elContainer);
+    contentEl.appendTo(elContainer);
+
+    container.append(elContainer);
+}
+
+
+function CreateDiv(cssClass) {
+    return createElement("div", cssClass);
+}
+
+function CreateSpan(cssClass) {
+    return createElement("span", cssClass);
+}
+
+function createElement(element, cssclass) {
+    var html = $("<" + element + "/>");
+    html.addClass(cssclass);
+    return html;
+}
